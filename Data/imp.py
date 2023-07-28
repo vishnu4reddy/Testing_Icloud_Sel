6661c5bff2a682652fd7cf965be6c63e2fabb262
vishnu4reddy <vreddy@msystechnologies.com>
2023-07-28 15:04:45 +0530

    data folder added

---
+++ Data/imp.py
@@ -0,0 +1,2 @@
+mail = "vishnuvardhanuv36@gmail.com"
+password = "Chandra@12"
--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from Data.imp import mail, password
 
 
 def test_bob():
@@ -14,9 +15,9 @@ def test_bob():
     driver.find_element(
         By.XPATH, "//div[@id='sign_in_form']/div/div/div/div/div/input").click()
     driver.find_element(
-        By.XPATH, "//div[@id='sign_in_form']/div/div/div/div/div/input").send_keys("vishnuvardhanuv36@gmail.com")
+        By.XPATH, "//div[@id='sign_in_form']/div/div/div/div/div/input").send_keys(mail)
     driver.find_element(By.XPATH, "//button[@id='sign-in']/i").click()
     driver.find_element(
-        By.XPATH, "//div[@id='sign_in_form']/div/div/div[2]/div/div/input").send_keys("Chandra@12")
+        By.XPATH, "//div[@id='sign_in_form']/div/div/div[2]/div/div/input").send_keys(password)
     driver.find_element(By.XPATH, "//button[@id='sign-in']/i").click()
     print("ended")
